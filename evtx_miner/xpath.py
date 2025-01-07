3d597e1cc935b56f9234743022d9b10693e1bc8d
Khalid Grandi <kh.grandi@gmail.com>
2025-01-07 19:08:00 +0100

    fix: fix xpath

--- evtx_miner/xpath.py
+++ evtx_miner/xpath.py
@@ -4,7 +4,13 @@ def xpath(tree, _xpath_query, cast=None):
         return None
 
     result = result[0]
-    result = (getattr(result, 'text', None) or str(result)).strip()
+    # print(_xpath_query, type(result), hasattr(result, 'tag'), hasattr(result, 'text'), dir(result))
+    if hasattr(result, 'text'):
+        result = result.text.strip() if result.text else result.text
+    else:
+        result = str(result).strip()
+    # result = (getattr(result, 'text', None) or str(result)).strip()
+
     if result and cast:
         return cast(result)
-    return result.strip() or None
+    return result
